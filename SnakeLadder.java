6cbb60410b197f8e5bdb74100bc2ef6ba1438a03
unknown <aboli2381@gmail.com>
2020-12-05 11:28:14 +0530

    UC1

---
+++ SnakeLadder.java
@@ -0,0 +1,20 @@
+
+public class SnakeLadder {
+	public int initializePosition()
+	{
+		int position=0;
+		if(position==0)
+		{
+			System.out.println("Player is at Position 0");
+		}
+		return position;
+	}
+	public static void main(String args[])
+	{
+		SnakeLadder snakeLadder=new SnakeLadder();
+		snakeLadder.initializePosition();
+		
+		
+	}
+
+}
